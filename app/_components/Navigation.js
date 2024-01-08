2fcbd602162562f9d7dc662415bb558afdded454
Vinay Selukar <vselukar2102@gmail.com>
2024-01-08 21:52:41 +0530

    Ui-left nav complete

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -6,35 +6,35 @@ import { navItems } from "../../data/navItems";
 
 export const Navigation = () => {
   return (
-    <aside
-      className={
-        "group/sidebar h-full bg-secondary overflow-y-auto relative flex gap-[24px] w-60 flex-col z-[99999] bg-[#1E2640] py-[16px] px-[10px]"
-      }
-    >
-      <NavigationItem
-        dropdown={true}
-        image={companyImage}
-        name="Nishyan"
-        linkName="Visit store"
-        linkRedirect="#"
-      />
-      <div className="flex flex-col gap-[4px]">
-        {navItems.map((item, index) => (
-          <NavigationItem
-            key={index}
-            navItem={true}
-            iconPath={item.svgPath}
-            buttonTxt={item.name}
-            linkRedirect="#"
-            onClick={() => {}}
-          />
-        ))}
+    <aside className="group/sidebar min-h-full bg-secondary relative flex w-[224px] flex-col justify-between z-[99999] bg-[#1E2640] py-[16px] px-[10px]">
+      <div className="flex flex-col gap-[24px] w-[208px]">
+        <NavigationItem
+          dropdown={true}
+          image={companyImage}
+          name="Nishyan"
+          linkName="Visit store"
+          linkRedirect="#"
+        />
+        <div className="flex flex-col gap-[4px]">
+          {navItems.map((item, index) => (
+            <NavigationItem
+              key={index}
+              navItem={true}
+              iconPath={item.svgPath}
+              buttonTxt={item.name}
+              linkRedirect="#"
+              onClick={() => {}}
+            />
+          ))}
+        </div>
       </div>
-      <NavigationItem
-        baseNav={true}
-        baseNavName="Available credits"
-        availableCredits="222.10"
-      />
+      <div className="">
+        <NavigationItem
+          baseNav={true}
+          baseNavName="Available credits"
+          availableCredits="222.10"
+        />
+      </div>
     </aside>
   );
 };
